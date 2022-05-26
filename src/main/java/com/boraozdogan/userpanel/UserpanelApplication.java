390c77f7f51ce86074c8491c24790ff48e8a52ec
Bozdogan <boraozdogan99@gmail.com>
2022-05-26 20:35:49 +0300

    INITIAL COMMIT

---
+++ src/main/java/com/boraozdogan/userpanel/UserpanelApplication.java
@@ -0,0 +1,13 @@
+package com.boraozdogan.userpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserpanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserpanelApplication.class, args);
+	}
+
+}

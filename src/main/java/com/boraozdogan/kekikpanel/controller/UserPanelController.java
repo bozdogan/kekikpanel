37fcb5bae2c631574309ca64504bb15de1bc518b
Bozdogan <boraozdogan99@gmail.com>
2022-05-27 22:41:15 +0300

    call internal API directly

--- src/main/java/com/boraozdogan/kekikpanel/controller/UserPanelController.java
+++ src/main/java/com/boraozdogan/kekikpanel/controller/UserPanelController.java
@@ -1,15 +1,15 @@
 package com.boraozdogan.kekikpanel.controller;
 
+import com.boraozdogan.kekikpanel.api.TasksRoute;
 import com.boraozdogan.kekikpanel.model.Task;
+import com.boraozdogan.kekikpanel.model.TaskRequestModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 
 @Controller
@@ -21,20 +21,27 @@ public class UserPanelController {
     @Value("${boz.app.apiurl}")
     private String apiURL;
 
+    @Autowired
+    private TasksRoute tasksRoute;
+
     @PostMapping("/user/createtask")
     public String createTask(
             @RequestParam("owner") String owner,
             @RequestParam("body") String body
     ) {
-        // NOTE(bora): Make internal API call over HTTP
-        var restTemplate = new RestTemplate();
-        var response = restTemplate.postForObject(
-                apiURL + "/tasks",
-                Map.of("owner", owner,"body", body),
-                Task.class);
+//        // NOTE(bora): Make internal API call over HTTP
+//        var restTemplate = new RestTemplate();
+//        var response = restTemplate.postForObject(
+//                apiURL + "/tasks",
+//                Map.of("owner", owner,
+//                        "body", body),
+//                Task.class);
+
+        // NOTE(bora): Call internal API directly
+        var response = tasksRoute.newTask(
+                new TaskRequestModel(owner, body));
 
         logger.info("Record added: {}", response);
-
         return "user_panel";
     }
 

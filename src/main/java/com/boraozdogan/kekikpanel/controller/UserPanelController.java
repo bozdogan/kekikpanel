77ffb3a81c25022b706d428634a4487edc53a584
Bozdogan <boraozdogan99@gmail.com>
2022-05-27 21:42:14 +0300

    implement /user/createtask path

--- src/main/java/com/boraozdogan/kekikpanel/controller/UserPanelController.java
+++ src/main/java/com/boraozdogan/kekikpanel/controller/UserPanelController.java
@@ -1,12 +1,38 @@
 package com.boraozdogan.kekikpanel.controller;
 
+import com.boraozdogan.kekikpanel.model.Task;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 
 @Controller
 public class UserPanelController {
     @Value("${boz.app.name}")
     private String appName;
+
+    @Value("${boz.app.apiurl}")
+    private String apiURL;
+
+    @PostMapping("/user/createtask")
+    public String createTask(
+            @RequestParam("owner") String owner,
+            @RequestParam("body") String body
+    ) {
+        // NOTE(bora): Make internal API call over HTTP
+        var restTemplate = new RestTemplate();
+        var response = restTemplate.postForObject(
+                apiURL + "/tasks",
+                Map.of("owner", owner,"body", body),
+                Task.class);
+
+        System.out.println("[UserPanelController#createTask]:: Record added: " + response);
+
+        return "user_panel";
+    }
 
 }
